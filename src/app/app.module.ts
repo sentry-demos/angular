33de9ff703e0234fc1f0bed101c20aef2686d57a
vudngo <vudngo@gmail.com>
2018-01-11 06:27:54 -0800

    update dsn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 
-/*
+
 Raven
   .config('https://60443a2c0a0c45599b1387046605bc88@sentry.io/269722')
   .install();
@@ -15,7 +15,7 @@ export class RavenErrorHandler implements ErrorHandler {
     Raven.captureException(err.originalError || err);
   }
 }
-*/
+
 
 @NgModule({
   declarations: [
