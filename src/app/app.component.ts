61e4476badf3aea8a90425e079c346480b50f5a0
Neil Manvar <neil@sentry.io>
2018-04-18 10:36:29 -0600

    Revert "Correct index"
    This reverts commit 85baf91ff2a72bd28adf0bdc55b6dfcf25e795ae.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,7 @@ export class AppComponent {
   // ERRORS
   notAFunctionError() {
     var someArray = [{ func: function () {}}];
-    someArray[0].func();
+    someArray[1].func();
   }
 
   uriError() {
