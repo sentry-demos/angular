5c06af1db68ee5886bf8bf95d9f8e198616a05eb
sentryadam0000345 <adam.toth-fejel@sentry.io>
2020-10-14 10:35:15 -0700

    Angular Demo Is Now Working With Performance.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Integrations } from "@sentry/tracing";
+import * as Sentry from "@sentry/angular";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+
+//import { AppModule } from "./app/app.module";
+
+Sentry.init({
+  dsn: "https://b38e0f5adaf845008633151efaeef0ea@o87286.ingest.sentry.io/5462755" ,
+  integrations: [
+    // Registers and configures the Tracing integration,
+    // which automatically instruments your application to monitor its
+    // performance, including custom Angular routing instrumentation
+    new Integrations.BrowserTracing({
+      tracingOrigins: ["localhost", "http://localhost:4200/"],
+      routingInstrumentation: Sentry.routingInstrumentation,
+
+    }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+export class SentryErrorHandler implements ErrorHandler {
+  handleError(err:any) : void {
+    Sentry.captureException(err.originalError || err);
+  }
+}
+
 
 @NgModule({
   declarations: [
@@ -10,9 +42,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [{ provide: ErrorHandler, useClass: SentryErrorHandler }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
